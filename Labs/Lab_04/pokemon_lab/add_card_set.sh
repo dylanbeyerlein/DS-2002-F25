c0fd520e84f8006fa5f4d988fbaf3dab246c93ee
Dylan Beyerlein <wqa8hw@virginia.edu>
2025-10-21 18:28:11 +0000

    Lab 4 - Step 1: Bash Scripts for API and File Management

---
+++ Labs/Lab_04/pokemon_lab/add_card_set.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+echo "Enter TCG Card Set ID (eg., base1, base4): "
+read SET_ID
+
+if [ -z "$SET_ID" ]; then
+    echo "Error: Set ID cannot be empty." >&2
+    exit 1
+fi
+
+echo "Fetching data for card set: '$SET_ID'..."
+
+curl -s "https://api.pokemontcg.io/v2/cards?q=set.id:${SET_ID}&page=1&pageSize=250" > "card_set_lookup/${SET_ID}.json"
+
+echo "Saved at card_set_lookup/${SET_ID}.json"
